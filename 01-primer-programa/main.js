624e71f6839b819b3f20073670025d75dabad055
Juan8516 <juan.ind85@gmail.com>
2025-02-09 16:47:54 -0500

    Reapply "variables"
    This reverts commit 918f88f7a67bb7ed5cbde5aa7018daabb45793a8.

---
+++ 01-primer-programa/main.js
@@ -0,0 +1,14 @@
+alert('Hola, esta es una primera ventana')
+alert('Esta es una ventana siguiente')
+
+console.log('Mensaje en la consola ... ')
+
+//Comentarios
+
+//1. Cometario de una linea
+//alert('Alerta comentada ... ')
+
+//2. Comentarios multilinea
+/*  alert('Alerta 1')
+    alert('Alerta 1')
+    alert('Alerta 1')  */
