0902b8dd73e8a4a2b1f07595a26afff9e04ad5ef
Juan8516 <juan.ind85@gmail.com>
2025-02-09 17:18:17 -0500

    constantes
    - Uso constantes
    - Demostracion de intento de cambio
    - Error generado en consola con intento de cambio

---
+++ 03-constantes/script.js
@@ -0,0 +1,15 @@
+//Constantes
+//Variable cuyo contenido nunca se puede cambiar
+
+let nombre = 'Juan Manuel'
+let apellidos = 'Salazar'
+
+//Constantes
+const cedula = 1043786553
+
+//Se trata de sobreescribir la constante (la intencion es mostrar el error)
+cedula = 1043786456
+
+
+console.log(nombre, apellidos)
+alert(cedula)
