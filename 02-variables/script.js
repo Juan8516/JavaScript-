145a18f772eb8b75842aa65dd80118eb541d3fe2
Juan8516 <juan.ind85@gmail.com>
2025-02-09 16:46:16 -0500

    variables
    - Declaracion de variables
    -Sobreescribir variables

---
+++ 02-variables/script.js
@@ -0,0 +1,20 @@
+//Variables
+//Caja donde se puede guardar cualquier tipo de informacion
+
+//Tipo de variables
+let heroe = 'Batman'
+let universo = 'DC'
+let edad = 50
+
+//Concatenar
+let heroe_y_universo = heroe + " es un personaje de " + universo
+
+//Sobreescribir las variables
+heroe = 'Superman'
+universo = 'DComics'
+edad = 40
+
+//Mostrar
+console.log(heroe, universo, edad)
+alert(heroe_y_universo)
+
