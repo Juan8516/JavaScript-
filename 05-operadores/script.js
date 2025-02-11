44d561812939b9d336d1832da0f795b5e0b35d7b
Juan8516 <juan.ind85@gmail.com>
2025-02-11 17:58:40 -0500

    Operador de asignacion
    - Se muestra asignacion normal de un numero
    - Se realizan operaciones compuestas, suma, resta, multiplicacion y division
    - Distintas maneras de mostras en consola, error y warning

--- 05-operadores/script.js
+++ 05-operadores/script.js
@@ -13,6 +13,7 @@ console.log(`El tipo de dato de heroe es: ${typeof(heroe)}`)
 let numero1 = 43
 let numero2 = 12
 
+//----Operaciones basicas
 //Suma
 let suma = numero1 + numero2
 //Resta
@@ -33,7 +34,23 @@ console.log('Division: ' + division)
 console.log('Resto: ' + resto)
 console.log('Potencia: ' + potencia)
 
-//Asignacion
+//----Asigancion
+let elNumero = 17
+
+//Primera forma - Asignación de un número con una operación de suma
+elNumero = 17 + 3 // elNumero ahora vale 20
+
+//Segunda forma - Operaciones de asignación compuesta
+elNumero += 3 // Suma 3 al valor actual (20 + 3 = 23)
+elNumero -= 3 // Resta 3 al valor actual (23 - 3 = 20)
+elNumero *= 3 // Multiplica por 3 (20 * 3 = 60)
+elNumero /= 3 // Divide por 3 (60 / 3 = 20)
+
+//Distintas maneras de mostrar en consola
+console.log(elNumero)    //Muestra el valor en la consola de manera normal
+console.error(elNumero) //Muestra el valor como un error en la consola
+console.warn(elNumero) //Muestra el valor como una advertencia en la consola
+
 //Comparacion
 //Logisticos
 //Cadena
