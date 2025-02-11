620cbe33cf78d31c9f87833c85fd1f0211f99da6
Juan8516 <juan.ind85@gmail.com>
2025-02-10 19:33:07 -0500

    Operador typeof
    - Se muestran ejemplos de como mostrar el tipo de datos con typeof

---
+++ 05-operadores/script.js
@@ -0,0 +1,10 @@
+//Operador typeof
+
+let frase = 'Juan Manuel Salazar DEV'
+let id
+let heroe = {nombre: 'Superman', edad: 40}
+
+//Typeof - muestra el tipo de dato de la variable
+console.log(`El tipo de dato de la frase es: ${typeof(frase)}`)
+console.log(`El tipo de dato del ID es: ${typeof(id)}`)
+console.log(`El tipo de dato de heroe es: ${typeof(heroe)}`)
