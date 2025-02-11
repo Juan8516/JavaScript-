b943875822613ecc3bcef7cf5ca2d93de6ca7a4c
Juan8516 <juan.ind85@gmail.com>
2025-02-11 17:24:33 -0500

    Operadores Basicos
    Se realizan operaciones basicas:
    - Suma, Resta, multiplicacion, division, resto y potenciacion.
    - Se muestran por consola.

--- 05-operadores/script.js
+++ 05-operadores/script.js
@@ -8,3 +8,33 @@ let heroe = {nombre: 'Superman', edad: 40}
 console.log(`El tipo de dato de la frase es: ${typeof(frase)}`)
 console.log(`El tipo de dato del ID es: ${typeof(id)}`)
 console.log(`El tipo de dato de heroe es: ${typeof(heroe)}`)
+
+//-------Operadores matematicos (matematicas)------//
+let numero1 = 43
+let numero2 = 12
+
+//Suma
+let suma = numero1 + numero2
+//Resta
+let resta = numero1 - numero2
+//Multiplicacion
+let producto = numero1 * numero2
+//Division
+let division = numero1 / numero2
+//Resto
+let resto = numero1 % numero2
+//Potencia
+let potencia = numero1 ** numero2
+
+console.log('Suma: ' + suma)
+console.log('Resta: ' + resta)
+console.log('Producto: ' + producto)
+console.log('Division: ' + division)
+console.log('Resto: ' + resto)
+console.log('Potencia: ' + potencia)
+
+//Asignacion
+//Comparacion
+//Logisticos
+//Cadena
+//Incremento y decremento
