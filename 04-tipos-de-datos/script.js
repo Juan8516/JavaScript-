9db0a96313950eeb9d4bcbe4fda4badc288f9be3
Juan8516 <juan.ind85@gmail.com>
2025-02-10 19:11:27 -0500

    Tipos de datos
    Se muestran tipos de datos como:
    - Enteros, float, arrays , objetos, null

---
+++ 04-tipos-de-datos/script.js
@@ -0,0 +1,22 @@
+//Tipos de datos
+
+//String o cadena
+let nombre = 'Juan Manuel'
+
+//Entero
+let anio = 2025
+
+//Decimal o float, double
+let interes = 2.7
+
+//Booleano
+let mayorEdad = true
+
+//Undifined - null variable no inicializada (intencionalmente)
+let vacio
+
+//Array o arreglo
+let frutas = ['Fresa', 'Durazno', 'Naranja']
+
+//Objeto, Clave - valor
+let heroe = {nombre: 'Flash', universo: 'DComics'}
