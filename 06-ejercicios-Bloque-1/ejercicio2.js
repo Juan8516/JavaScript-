68c810851e8224e0758fc720acd1650e79850256
Juan8516 <juan.ind85@gmail.com>
2025-02-15 14:05:09 -0500

    Ejercicio 2
    - Calculo de cantidad dee galletas en determinado numero de cajas.

---
+++ 06-ejercicios-Bloque-1/ejercicio2.js
@@ -0,0 +1,14 @@
+/*
+    ----Ejercicio 2----
+
+    -Hay 7 cajas con 8 galletas rellenas de chocolate cada una.
+    -Cuantas galletas hay en total ?
+
+*/
+
+let cajas = 7
+let galletasPorCaja = 8
+
+let totalGalletas = cajas * galletasPorCaja
+
+console.log(`El total de galletas es: ${totalGalletas}`)
