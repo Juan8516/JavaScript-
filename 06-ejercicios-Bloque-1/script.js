80adca3bcf0fb7610383612b560fa2fffcc74b34
Juan8516 <juan.ind85@gmail.com>
2025-02-15 13:50:01 -0500

    Ejercicio 1
    - Calculo del tiempo recorrido por una nave, para llegar a la luna.

---
+++ 06-ejercicios-Bloque-1/script.js
@@ -0,0 +1,15 @@
+/*
+    ----Ejercicio 1----
+
+    -Calcula cuantas horas le llevaria llegar a la luna a una nave espacial.
+    -La distancia desde la tierra hasta la luna es de 384.000 kilometros
+    -La velocidad de la nave es de 1225 kilometros por hora
+    -Guarda el resultado en una variable.
+*/
+
+let distancia = 384000
+let velocidadNave = 1225
+
+let tiempoRecorrido = distancia/velocidadNave
+
+console.warn(`El tiempo de recorrido es: ${tiempoRecorrido} horas`)
