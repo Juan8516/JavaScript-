17106a465d860f64192e211bbeea165fe46b77af
Juan8516 <juan.ind85@gmail.com>
2025-02-16 12:50:39 -0500

    Ejercicio 3
    - Calculo del total de minutos en un determinado numero de horas.

---
+++ 06-ejercicios-Bloque-1/ejercicio3.js
@@ -0,0 +1,15 @@
+/*
+    ----Ejercicio 3----
+
+    -Cuantos minutos tienen 7 horas ?
+
+    Utiliza variables y operadores para resolver
+
+*/
+
+let horas = 7
+const minutosHora = 60
+
+let resultado = minutosHora * horas
+
+console.log(`En 7 horas hay un total de ${resultado} minutos`)
