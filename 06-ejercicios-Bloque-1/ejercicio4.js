b367f1d0481a6e1f27673edf64dd86f240180719
Juan8516 <juan.ind85@gmail.com>
2025-02-17 20:07:29 -0500

    Ejercicio 4
    - Calculo del peso de una jirafa despues de comer.

---
+++ 06-ejercicios-Bloque-1/ejercicio4.js
@@ -0,0 +1,16 @@
+/*
+    ----Ejercicio 4----
+
+    -Tenemos una jirafa en el zoo que pesa 1120 kilos
+     Se le da de comer 141 kilos de hojas frescas
+
+    Cuanto pesa ahora la jirafa, despues de comer ?
+
+*/
+
+let pesoJirafa = 1120
+let hojasKilos = 141
+
+let nuevoPeso = pesoJirafa + hojasKilos
+
+console.warn(`El peso nuevo de la jirafa es: ${nuevoPeso} kilos al comer ${hojasKilos}`)
