0e0b545f407f1144a8306b799c8d69bf232e793e
Juan8516 <juan.ind85@gmail.com>
2025-02-18 18:06:58 -0500

    Ejericicio 7
    - Calculo del radio de un circulo

---
+++ 06-ejercicios-Bloque-1/ejercicio7.js
@@ -0,0 +1,14 @@
+/*
+    ----Ejercicio 7----
+
+    - Tenemos un circulo con un diametro de 18cm.
+
+    En una alerta mostrar el radio del circulo.
+*/
+
+let perimetro = 18
+const PI = 3.1416 * 2
+
+let diametro = perimetro / PI
+
+alert(`El radio del circulo es: ${diametro}`)
