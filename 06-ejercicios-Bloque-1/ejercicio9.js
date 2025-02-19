dc10def217eec51fcd2fcedb77794d9b84230a5e
Juan8516 <juan.ind85@gmail.com>
2025-02-19 17:37:25 -0500

    Ejercicio 9
    - Convertir millas a kilometros

---
+++ 06-ejercicios-Bloque-1/ejercicio9.js
@@ -0,0 +1,15 @@
+/*
+    ----Ejercicio 9----
+
+    - Una milla tiene 1.6 kilometros
+
+    Cuantos kilometros tiene una autopista de 120 millas ?
+
+*/
+
+let millaEnKilometros = 1.6
+let longitudAutopista = 120
+
+let convertir = longitudAutopista * millaEnKilometros
+
+console.log(`En una autopista de 120 millas hay un total de ${convertir} kilometros`)
