4858aff87e1db948ae89f3386b241f65b2ff347f
Juan8516 <juan.ind85@gmail.com>
2025-02-26 18:56:52 -0500

    Ejercicio 11
    - Conversion de años humanos a años caninos

---
+++ 12-ejercicios-Bloque-2/ejercicio11.js
@@ -0,0 +1,14 @@
+/*
+    ----Ejercicio 11----
+
+    -Nuestros usuarios tienen perros
+    'Quieren saber que edad tienen sus mascotas.
+    *Un año humano equivale a siete de perro.
+
+    Preguntar a los usuarios que edad tiene el perro, y decir la edad canina
+*/
+
+let edadPerro = prompt('Ingresa la edad de tu perro (en tiempo normal)');
+let convertir = edadPerro * 7;
+
+alert(`La edad canina de tu perro es: ${convertir}`);
