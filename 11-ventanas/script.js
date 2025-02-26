9272984fe13a88814700adca7b1d63e06955d3f1
Juan8516 <juan.ind85@gmail.com>
2025-02-25 19:52:07 -0500

    Ventanas y alertas
    -  Mostrar info en ventanas
    -  Ventana de confirmacion
    -  Solicitar datos al usuario

---
+++ 11-ventanas/script.js
@@ -0,0 +1,11 @@
+//Alertas
+let mensaje = 'Ventana de alerta con JS';
+alert(mensaje);
+
+//Ventanas de confirmacion
+let confirmacion = confirm('Quieres continuar ?');
+console.log(confirmacion);
+
+//Ingreso de datos (usuario)
+let edad = prompt('Que edad tienes ?');
+console.log(edad)
