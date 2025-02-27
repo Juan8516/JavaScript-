80646db21f6066d40bc890182f07b88d99209dc7
Juan8516 <juan.ind85@gmail.com>
2025-02-26 20:07:01 -0500

    Ejercicio 12
    - Comparacion de dos numeros
    - Mostrar cual es mayor y cual es menor
    - Si son iguales
    - Verificacion si son numeros

---
+++ 12-ejercicios-Bloque-2/ejercicio12.js
@@ -0,0 +1,46 @@
+/*
+    ----Ejercicio 12----
+
+    --Programa que pida dos numeros, y diga
+    -Cual es mayor
+    -Cual es menor
+    -O sin son iguales
+
+    *Si el dato ingresado no es un numero o son menores a 0, volver a pedir
+
+*/
+
+let numero1 = parseInt(prompt('Ingresar 1er numero: '));
+let numero2 = parseInt(prompt('Ingresar 2do numero: '));
+
+while(numero1 <= 0 || numero2 <= 0 || isNaN(numero1) || isNaN(numero2)) {
+
+    numero1 = parseInt(prompt('Ingresar 1er numero: '));
+    numero2 = parseInt(prompt('Ingresar 2do numero: '));
+}
+
+if(numero1 === numero2) {
+
+    alert('Los numeros son iguales !!!!');
+
+} else if (numero1 > numero2) {
+
+    alert(`
+        El primer numero es mayor !!!!
+        El segundo numero es menor !!!!
+    `);
+
+} else if(numero1 < numero2) {
+
+    alert(`
+        El primer numero es menor !!!!
+        El segundo numero es mayor !!!!
+    `);
+
+} else {
+
+    alert('No has ingresado un numero')
+
+}
+
+
