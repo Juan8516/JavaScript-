db8582a783ad381c4c4b746e1baae6d779d73a11
Juan8516 <juan.ind85@gmail.com>
2025-02-27 20:13:24 -0500

    Ejercicio 14
    - Se selecciona un genero musical, y se muestra por consola.

---
+++ 12-ejercicios-Bloque-2/ejercicio14.js
@@ -0,0 +1,28 @@
+/*
+    ----Ejercicio 14----
+
+    --Dj no sabe que genero musical poner en la fiesta:
+    -Preguntar al usuario que genero quiere (pop, rock o rap).
+    -Segun lo que prefiera devuelve un mensaje diferente.
+
+*/
+
+let genero = prompt('Ingresa el genero que quieres escuchar: ')
+
+switch(genero) {
+
+    case 'pop':
+        console.log(`El genero seleccionado es ${genero}`);
+        break;
+    
+    case 'rock':
+        console.log(`El genero seleccionado es ${genero}`);
+        break;
+    
+    case 'rap':
+        console.log(`El genero seleccionado es ${genero}`);
+        break;
+
+        default:
+            console.error('Genero no encontrado !!!')
+}
