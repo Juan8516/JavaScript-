3ce9b1b9e22f56c77a983770c3849d197412c3c4
Juan8516 <juan.ind85@gmail.com>
2025-03-02 18:44:12 -0500

    Ejercicio 22
    - Cantidad de caramelos, en una bolsa

---
+++ 12-ejercicios-Bloque-2/ejercicio22.js
@@ -0,0 +1,20 @@
+/*
+    ----Ejercicio 22----
+
+    --Tenemos una bolsa de 37 caramelos
+    -Cada vez que se saca uno se va reduciendo
+    ✔️Con un bucle, realizar una sumulacion.
+
+*/
+
+let bolsa = 37;
+
+while(bolsa > 0) {
+
+    //Mostrar algo
+    console.warn(`Te acabas de comer un caramelo, quedan ${bolsa} caramelos en la bolsa`);
+
+    //Bolsa = bolsa - 1
+    bolsa--;
+
+}
