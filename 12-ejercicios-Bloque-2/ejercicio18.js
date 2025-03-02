d5c37829453d360bf2bd0b3c90b51d2eb85987ef
Juan8516 <juan.ind85@gmail.com>
2025-03-01 21:09:38 -0500

    Ejercicio 18
    - Usuario debe adivinar el numero, escogido en el codigo

---
+++ 12-ejercicios-Bloque-2/ejercicio18.js
@@ -0,0 +1,23 @@
+/*
+    ----Ejercicio 18----
+
+    --Tenemos una coleccion de numeros desde 1 al 17:
+    -El usuario tiene que adivinar cual es el elegido
+
+    🔮🪄Un programa que pueda adivinar el numero
+
+*/
+
+const numAdivinar = 8;
+let intento;
+
+while(intento != numAdivinar) {
+
+    intento = parseInt(prompt('Ingresar numero de 1 a 17'))
+
+    if(numAdivinar === intento) {
+        alert(`Correcto el numero era es el: ${intento}`)
+    }
+}
+
+
