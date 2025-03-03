2bb7db9dcca041aa9094b2ee0ff3c055e7d75279
Juan8516 <juan.ind85@gmail.com>
2025-03-02 19:26:09 -0500

    Ejercicio 23
    - Cuenta regresiva

---
+++ 12-ejercicios-Bloque-2/ejercicio23.js
@@ -0,0 +1,15 @@
+/*
+    ----Ejercicio 23----
+
+    --Un cohete va directo al espacio
+    -Crear una cuenta regresiva de 10 segundos
+    *Utilizar bucle for
+
+*/
+
+
+for(let contador = 10; contador >= 1; contador--) {
+
+    console.warn(`Quedan ${contador } segundos para el despegue`);
+
+}
