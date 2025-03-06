8b2db4943540206c4b3f1276e54af8180ffd593f
Juan8516 <juan.ind85@gmail.com>
2025-03-05 20:32:17 -0500

    Funciones
    - Parametros

--- 13-funciones/script.js
+++ 13-funciones/script.js
@@ -1,16 +1,19 @@
 //Funciones: son bloques de código reutilizables que ejecutan una tarea específica. Pueden recibir parámetros y 
 // devolver un resultado.
 
-function saludo() {
-    console.log("Atencion !!!!!!");
-    console.warn("Peligroooo !!!!!!")
-    console.error("Explosion inminente !!!!!!")
+function saludo(sector, ruta) {
+    console.log(`Atencion sector ${sector}!!!!!!`);
+    console.warn('Peligroooo !!!!!!');
+    console.error(`Explosion inminente tomar ${ruta}!!!!!!`);
+    console.log('------------------------')
 
     //JavaScript se usa dentro de una función para devolver un valor y finalizar su ejecución.
     return 'Alerta enviada correctamente !!!!!'
 }
 
-let mensajeFinal = saludo();
+let mensajeFinal = saludo(23, 'vias de evacuacion');
+let mensajeFinal1 = saludo(16, 'ruta subterranea');
+let mensajeFinal2 = saludo(85, 'ruta trasera');
 
 alert(mensajeFinal);
 
