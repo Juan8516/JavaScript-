09b3ec4daf49726c96f7ccfb3e72020778e8fc71
Juan8516 <juan.ind85@gmail.com>
2025-03-08 20:51:24 -0500

    Funciones
    - Ambitos o Scope

--- 14-mas-funciones/script.js
+++ 14-mas-funciones/script.js
@@ -26,24 +26,57 @@ setInterval(saludo, 2000);
 */
 
 //Funciones Callbacks - avanzado
-function restame(n1, n2, mostrar, multiPorTres) {
+// function restame(n1, n2, mostrar, multiPorTres) {
 
-    let resta = n1 - n2;
+//     let resta = n1 - n2;
 
-    mostrar(resta);
-    multiPorTres(resta);
+//     mostrar(resta);
+//     multiPorTres(resta);
 
-    return resta;
+//     return resta;
+// }
+
+// restame(100, 5, function(resultado) {
+
+//     console.log(resultado);
+
+// }, 
+// function(resultado) {
+
+//     console.log(resultado * 3);
+
+// });
+
+//Ambito de las variables
+
+/*Global = Una variable declarada fuera de cualquier función o bloque tiene ámbito global y 
+puede ser accedida desde cualquier parte del código.*/
+
+let rutaGit = ('https://github.com/Juan8516');
+
+function mostrarRuta() {
+
+    //Global - Llamado de la variable dentro de la funcion
+    console.warn(rutaGit);
+
+    //Local = Las variables declaradas dentro de una función solo pueden ser accedidas dentro de esa función. 
+    // No existen fuera de ella.
+    let nombre = ("Ruta Git Juan Manuel Salazar");
+
 }
 
-restame(100, 5, function(resultado) {
+//Local - Llamado de la variable fuera de la funcion
+console.error(nombre); //❌ Error: mensaje is not defined
+mostrarRuta();
 
-    console.log(resultado);
+/*Bloque = Con let y const, las variables tienen ámbito de bloque {}. Solo existen dentro del bloque en 
+el que fueron declaradas.*/
 
-}, 
-function(resultado) {
+if (true) {
 
-    console.log(resultado * 3);
+    let bloque = "Solo existo aquí";
+    console.log(bloque); // 👉 "Solo existo aquí"
+    
+}
 
-});
-
+console.log(bloque); // ❌ Error: bloque is not defined
