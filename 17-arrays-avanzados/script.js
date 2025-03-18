fa5d6bb580da205f5d95c1ad59a7f656fd2ce871
Juan8516 <juan.ind85@gmail.com>
2025-03-17 21:38:13 -0500

    Arrays
    - Uso de blucles: for, for OF, for IN, foreach, map

--- 17-arrays-avanzados/script.js
+++ 17-arrays-avanzados/script.js
@@ -28,3 +28,67 @@ for(let i = 0; i <= frameworks.length; i++) {
 }
 
 document.write("</ul>"); 
+
+//Bucles para recorrer arrays
+console.log('---------------------');
+console.log(' ');
+
+let actores = ['Arnold Swayeneger', 'Silvestre Stallone', 'Al Pacino', 'Robert De Niro', 'Leonadro Di Caprio', 'Adam Sandler'];
+
+console.log('!!!!!!!!!!Listado de actores!!!!!!!!!!');
+console.log(' ');
+
+//Bucle for 
+console.log('-Usando el bucle for-');
+
+for(contador = 0; contador < actores.length; contador++) {
+    
+    console.log(actores[contador]);
+    
+}
+console.log(' ');
+
+//Bucle for OF - mas limpio que el for
+console.log('-Usando el bucle for OF-mas limpio que el for');
+
+for(let actor of actores) {
+
+    console.log(actor);
+
+}
+console.log(' ');
+
+//Bucle for IN - Muestra el indice de cada elemento en el array
+console.log('-Usando el bucle for IN - Muestra el indice de cada elemento en el array');
+
+for(let indice in actores) {
+
+    console.log(indice, actores[indice]);
+
+}
+console.log(' ');
+
+//Bucle foreach - Recorre el array por medio de una funcion
+console.log('-Usando el bucle foreach-Recorre el array por medio de una funcion');
+
+actores.forEach((indice, actor) => {
+
+    console.log(indice, actor);
+
+})
+console.log(' ');
+
+//Bucle map - Recorre el array y devuelve un nuevo array no modifica el original
+console.log('-Usando el bucle map-devuelve un nuevo array no modifica el original');
+
+let nuevosActores = actores.map(actor => {
+
+    console.log(actor);
+
+    return actor + ' Es un actor '
+
+})
+
+console.log(nuevosActores);
+
+
