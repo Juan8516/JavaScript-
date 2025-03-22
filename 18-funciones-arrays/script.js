6314b3a14a3662d58570fb718be199a3927eccdb
Juan8516 <juan.ind85@gmail.com>
2025-03-22 16:35:18 -0500

    Arrays
    - Funciones con arrays III: sort, every, flat, copyWithin, Array.from, destructuracion, ...spread, toString, split, join

--- 18-funciones-arrays/script.js
+++ 18-funciones-arrays/script.js
@@ -111,3 +111,76 @@ console.log('');
 let buscar1 = f1Teams.findIndex(team => team.length > 8);
 console.log(`Buscar el indice del elemento (primera coincidencia con las de 8 letras): ${buscar1}`);
 console.log('');
+
+//some = verifica si al MENOS un elemento de un array cumple con una condición y devuelve true o false
+let letrasE = f1Teams.some(team => team.includes('F'));
+console.log(`Verifica si al menos hay una coincidencia, letra(F) devuelve booleano: ${letrasE}`);
+console.log('');
+
+//every = verifica si TODOS los elementos de un array cumplen con una condición y devuelve true o false
+let letrasA = f1Teams.every(team => team.includes('a'));
+console.log(`Verifica si todos los elementos tienen la coincidencia, letra(A) devuelve booleano: ${letrasA}`);
+console.log('');
+
+//flat =  aplana un array, eliminando la estructura de sub-arrays y devolviendo un array plano
+let anios = [
+    [1900, 1992, 1993],
+    [2000, 2005, 2006],
+    [2010, 2012, 2014, [2020, 2021, [2025, 2026,2027]]]
+];
+
+let plana = anios.flat(Infinity);
+console.log(`Array multidimencional aplanado: ${plana}`);
+console.log('');
+
+//copyWithin = copia una parte del array dentro del mismo array, sin cambiar su longitud
+let numeros = [10, 20, 30, 40, 50];
+numeros.copyWithin(0, 2, 4);
+console.log(`copia de array conservando longitud: ${numeros}`); 
+console.log('');
+
+//Array.from = crea un nuevo array a partir de un objeto iterable (como strings, sets, nodelists, etc.) o un objeto 
+//con la propiedad length.
+let nombre = 'Juan Manuel';
+let arrayString = Array.from(nombre)
+console.log(`Conversion a array desde un string: ${arrayString}`); 
+console.log('');
+
+//Destructuring =  extraer valores de arrays u objetos y asignarlos a variables de forma sencilla
+let [uno, dos, tres] = [1, 2, 3];
+console.log('Primera forma de destructuracion:');
+console.log(dos);
+console.log('');
+
+let [actor, ...restoDeActores] = ['Robert de Niro', 'Al Pacino', 'Bruce Willis', 'Macaulay Culkin'];
+console.log('Segunda forma de destructuracion:');
+console.log(actor);
+console.log(restoDeActores);
+console.log('');
+
+//Expandir arrays = ...(spread) permite expandir elementos de arrays, objetos o argumentos en funciones
+let array1 = [4, 5, 6];
+let array2 = [1, 2, 3];
+
+let arrayCompleto = [...array1, ...array2];
+
+console.log('Expansion de array con spread:');
+console.log(arrayCompleto);
+console.log('');
+
+//toString = convierte un array, número u objeto en una cadena de texto (string)
+console.log('Convertir array a string con toString:');
+console.log(f1Teams.toString());
+console.log('');
+
+//split = divide un string en un array de subcadenas, usando un separador especificado
+let frase = 'La formula 1 es un deporte muy interesante';
+let arrayFrase = frase.split(' ');
+console.log('Convertir string a array:');
+console.log(arrayFrase);
+console.log('');
+
+//join = convierte un array en un string, uniendo los elementos con un separador especificado
+let slug = arrayFrase.join(' ').toLocaleLowerCase();
+console.log('Convertir array a un string:');
+console.log(slug);
