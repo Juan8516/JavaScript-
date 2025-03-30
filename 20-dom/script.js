e6b2969ad5380cde5815f0c736a6853443970158
Juan8516 <juan.ind85@gmail.com>
2025-03-29 21:01:00 -0500

    DOM
    - Uso de estilos desde el archivo styles.css

--- 20-dom/script.js
+++ 20-dom/script.js
@@ -4,7 +4,11 @@
 
 const caja = document.getElementById("caja"); // Selecciona el elemento con id "caja"
 
+// Configuración del contenido y estilo del elemento con id "caja"
 caja.innerHTML = "<h2>Articulos deportivos</h2>"; // Cambia el contenido HTML del elemento seleccionado
+
+/*
+//Directo desde el script
 caja.style.backgroundColor = "lightblue"; // Cambia el color de fondo del elemento seleccionado
 caja.style.maxWidth = "50%"; // Cambia el ancho máximo del elemento seleccionado
 caja.style.margin = "0, auto"; // Cambia el margen del elemento seleccionado
@@ -12,6 +16,10 @@ caja.style.padding = "35px"; // Cambia el relleno del elemento seleccionado
 caja.style.border = "1px solid black"; // Cambia el borde del elemento seleccionado
 caja.style.textAlign = "center"; // Cambia la alineación del texto del elemento seleccionado
 caja.style.fontFamily = "Arial"; // Cambia la fuente del texto del elemento seleccionado
+*/
+
+//Desde el CSS
+caja.classList.add("cajita"); // Agrega la clase "caja" al elemento seleccionado
 
 console.log(caja.innerText); // Imprime el elemento en la consola
 
