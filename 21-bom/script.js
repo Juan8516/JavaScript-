186fdf7b1c3eabf5b7a62210ca6df3b70655c42d
Juan8516 <juan.ind85@gmail.com>
2025-04-04 18:20:56 -0500

    BOM
    - Uso de alerts, confirm y prompt

--- 21-bom/script.js
+++ 21-bom/script.js
@@ -1,2 +1,6 @@
 //BOM - Browser Object Model
 
+//Alertas y dialogos
+alert("Hola Mundo desde un alert"); // Muestra un mensaje de alerta al usuario
+confirm("¿Quieres continuar?") //Devuelve true o false  
+prompt("¿Cuál es tu nombre?") //Devuelve el valor ingresado por el usuario
