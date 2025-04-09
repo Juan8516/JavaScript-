cfa3eed7b9e7b333f141ca6e0e733a4753a279fa
Juan8516 <juan.ind85@gmail.com>
2025-04-08 21:03:28 -0500

    EVENTOS
    - Uso de object Event

--- 22-eventos/script.js
+++ 22-eventos/script.js
@@ -7,11 +7,14 @@
  * "Hola, has hecho click en el botón" y se registra el mismo mensaje en la consola.
  *
  */
-let clickBoton = document.querySelector("#boton");
+let clickBoton = document.querySelector("#boton"); // Selecciona el primer elemento con el ID "boton"
 
-clickBoton.addEventListener("click", () => {
+clickBoton.addEventListener("click", (event) => {
 
-    alert("Hola, has hecho click en el botón");
-    console.log("Hola, has hecho click en el botón");
+    alert("Hola, has hecho click en el botón"); // Muestra una alerta al usuario
+    console.log("Hola, has hecho click en el botón");// Registra un mensaje en la consola
+    console.log(event); // Muestra el evento en la consola
+    console.log(event.target); // Muestra el elemento que disparó el evento en la consola
+    console.log(event.type); // Muestra el tipo de evento en la consola
     
 });
