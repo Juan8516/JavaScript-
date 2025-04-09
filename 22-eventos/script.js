dad2474cccdba2b33e511042aa66cdc15097f727
Juan8516 <juan.ind85@gmail.com>
2025-04-08 20:55:50 -0500

    EVENTOS
    - Uso de addEventListener

--- 22-eventos/script.js
+++ 22-eventos/script.js
@@ -1,1 +1,17 @@
 //Eventos
+
+/**
+ * Selecciona todos los elementos con el ID "botocito" y les agrega un evento de click.
+ * 
+ * Cuando se hace clic en el botón, se muestra una alerta con el mensaje 
+ * "Hola, has hecho click en el botón" y se registra el mismo mensaje en la consola.
+ *
+ */
+let clickBoton = document.querySelector("#boton");
+
+clickBoton.addEventListener("click", () => {
+
+    alert("Hola, has hecho click en el botón");
+    console.log("Hola, has hecho click en el botón");
+    
+});
