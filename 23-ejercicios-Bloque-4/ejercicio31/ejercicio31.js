8b03530127026e0d6a027a8ed8e5579b5470619a
Juan8516 <juan.ind85@gmail.com>
2025-04-15 21:12:45 -0500

    Ejercicio 31
    - Semaforo
    - Dibujo de semaforo con html y css

---
+++ 23-ejercicios-Bloque-4/ejercicio31/ejercicio31.js
@@ -0,0 +1,16 @@
+/* 
+ Ejercicio 31:
+
+ Crea un semaforo con tres luces (rojo, amarillo y verde).
+ Haz los circulos del semaforo con html y css.
+
+ Cuando un peaton quiere cruzar la calle debe pulsar un boton.
+ Crea un boton para cambiar el color de las luces del semaforo.
+ Secuencialmente (de rojo a verdes, pasando por el amarillo).
+
+ Entonces,
+ El semaforo siempre empieza en rojo.
+ Cuando el peaton pulsa el boton, el semaforo cambia a amarillo.
+ Al hacer click otra vez, el semaforo cambia a verde.
+*/
+
