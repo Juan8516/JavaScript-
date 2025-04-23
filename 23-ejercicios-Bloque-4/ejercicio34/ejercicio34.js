a0090d4e9cb0bfcdbb5439041f82bb421fcc9f2f
Juan8516 <juan.ind85@gmail.com>
2025-04-22 22:20:00 -0500

    Ejercicio 34
    - Ventanas web
    - Preparacion de archivos

---
+++ 23-ejercicios-Bloque-4/ejercicio34/ejercicio34.js
@@ -0,0 +1,8 @@
+/*
+Ejercicio 34
+
+Crea un programa que abra una nueva ventana con una web
+pero con diferentes tamaños.
+
+Hacer que el usuario pueda elegir tres tamaños diferentes de ventana: (pequeña, mediana y grande).
+*/
