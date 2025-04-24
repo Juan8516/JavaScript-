ea9ab113592c5b977c9e1ebcc49e325989fdc93f
Juan8516 <juan.ind85@gmail.com>
2025-04-23 22:28:27 -0500

    Ejercicio 34
    - Ventanas web
    - Funcionalidad para abrir ventanas

--- 23-ejercicios-Bloque-4/ejercicio34/ejercicio34.js
+++ 23-ejercicios-Bloque-4/ejercicio34/ejercicio34.js
@@ -6,3 +6,34 @@ pero con diferentes tamaños.
 
 Hacer que el usuario pueda elegir tres tamaños diferentes de ventana: (pequeña, mediana y grande).
 */
+
+let ventanaPequena = document.querySelector("#ventanaPequena");
+let ventanaMediana = document.querySelector("#ventanaMediana");
+let ventanaGrande = document.querySelector("#ventanaGrande");
+
+let abrirVentana = (altura, anchura) => {
+
+    window.open("https://developer.mozilla.org/es/",
+                "_blank",
+                `width=${altura}, height=${anchura}`);
+    
+    return true;
+};
+
+ventanaPequena.addEventListener('click', () => {
+
+    abrirVentana(300, 400);
+
+});
+
+ventanaMediana.addEventListener('click', () => {
+
+    abrirVentana(500, 600);
+
+});
+
+ventanaGrande.addEventListener('click', () => {
+
+    abrirVentana(800, 800);
+
+});
