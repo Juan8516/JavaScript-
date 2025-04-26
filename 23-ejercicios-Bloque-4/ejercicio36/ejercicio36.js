f32afd0200e79aac212e49320441f0c65ffec268
Juan8516 <juan.ind85@gmail.com>
2025-04-25 21:26:30 -0500

    Ejercicio 36
    - Contador teclas
    - Funcionalidad contador de pulsaciones

--- 23-ejercicios-Bloque-4/ejercicio36/ejercicio36.js
+++ 23-ejercicios-Bloque-4/ejercicio36/ejercicio36.js
@@ -7,3 +7,13 @@ dentro de una caja.
 Muestra el numero de teclas pulsadas en tiempo real en la web.
 */
 
+let contador = document.querySelector("#contador");
+let cajaPulsaciones = document.querySelector("#pulsaciones");
+let pulsacionesReales = 0;
+
+contador.addEventListener("input", () => {
+
+    pulsacionesReales++;
+    cajaPulsaciones.textContent = `Teclas pulsadas: ${pulsacionesReales}`;
+
+});
