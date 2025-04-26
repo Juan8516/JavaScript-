2c682ad97b70c6dbd50373847cf83bc9619412ed
Juan8516 <juan.ind85@gmail.com>
2025-04-25 21:12:21 -0500

    Ejercicio 36
    - Contador teclas
    - Preparacion de archivos

---
+++ 23-ejercicios-Bloque-4/ejercicio36/ejercicio36.js
@@ -0,0 +1,9 @@
+/*
+Ejercicio 36
+
+Crea un programa de texto que cuente cuantas teclas ha pulsado el usuario,
+dentro de una caja.
+
+Muestra el numero de teclas pulsadas en tiempo real en la web.
+*/
+
