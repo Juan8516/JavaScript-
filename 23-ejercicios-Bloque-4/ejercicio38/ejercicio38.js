444e4d68641ff4bd5ec9b8bf81b3e2570960f50b
Juan8516 <juan.ind85@gmail.com>
2025-04-27 16:10:04 -0500

    Ejercicio 38
    - Validacion Formulario
    - Preparacion de archivos

---
+++ 23-ejercicios-Bloque-4/ejercicio38/ejercicio38.js
@@ -0,0 +1,9 @@
+/*
+Ejercicio 38
+
+Valida el formulario del ejercicio 37, con JS, usando el DOM, funciones y metodos.
+
+El nombre y apellidos no deben estar vacios.
+
+La edad debe NO ser menos que 0, y debe ser siempre un numero.
+*/
