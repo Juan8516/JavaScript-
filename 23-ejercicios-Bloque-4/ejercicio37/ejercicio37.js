93b69775e4fd3baf80bc9db89d3700a4e615d736
Juan8516 <juan.ind85@gmail.com>
2025-04-27 09:52:21 -0500

    Ejercicio 37
    - Formulario
    - Funcionalidad para envio y visualizacion de informacion en pantalla

--- 23-ejercicios-Bloque-4/ejercicio37/ejercicio37.js
+++ 23-ejercicios-Bloque-4/ejercicio37/ejercicio37.js
@@ -6,3 +6,25 @@ Añade un botón de enviar y usa el evento submit.
 Muestra los datos por pantalla cuando se termine de enviar el formulario.
 */
 
+const formulario = document.querySelector("#formulario");
+const infoContacto = document.querySelector("#info_contacto");
+
+let nombre =document.querySelector("#nombre");
+let apellidos =document.querySelector("#apellidos");  
+let edad =document.querySelector("#edad");
+
+let infoNombre =document.querySelector("#info_nombre strong");
+let infoApellidos =document.querySelector("#info_apellidos strong");  
+let infoEdad =document.querySelector("#info_edad strong");
+
+formulario.addEventListener("submit", (e) => {
+
+    e.preventDefault(); // Evita que el formulario se envíe y recargue la página
+
+    alert("Formulario enviado !!!!");
+    infoNombre.textContent = nombre.value;
+    infoApellidos.textContent = apellidos.value;
+    infoEdad.textContent = edad.value;
+
+});
+
