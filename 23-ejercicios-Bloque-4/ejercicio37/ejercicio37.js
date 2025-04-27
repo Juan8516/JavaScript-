6297182ade91b39173484295b65b64ff93577330
Juan8516 <juan.ind85@gmail.com>
2025-04-27 08:45:26 -0500

    Ejercicio 37
    - Formulario
    - Preparacion de archivos

---
+++ 23-ejercicios-Bloque-4/ejercicio37/ejercicio37.js
@@ -0,0 +1,8 @@
+/*
+Ejercicio 37
+
+Crea un formulario que pida al usuario su nombre, apellidos y edad.
+Añade un botón de enviar y usa el evento submit.
+Muestra los datos por pantalla cuando se termine de enviar el formulario.
+*/
+
