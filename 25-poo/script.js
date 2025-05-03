de908ecba47ae7102ce0d1d61eb90cdab69d7fec
Juan8516 <juan.ind85@gmail.com>
2025-05-03 16:45:46 -0500

    POO
    - Clases
    - Uso de getter y setter

--- 25-poo/script.js
+++ 25-poo/script.js
@@ -20,12 +20,40 @@ class Gato {
     presentar() {
         console.log(`Hola, soy ${this.nombre}, tengo ${this.edad} años y soy de la raza ${this.raza}.`); // Método de instancia
     }
+
+    getRaza() {
+        return this.raza; // Método de instancia
+    }
+
+    setRaza(nuevaRaza) {
+        this.raza = nuevaRaza; // Método de instancia
+    }
+
+    set guardarEdad(nuevaEdad) {
+        this.edad = nuevaEdad; // Método de instancia
+    }
+
+    get sacarEdad() {
+        return this.edad; // Método de instancia
+    }
+
 }
 
 let miGato1 = new Gato("Tanque", 2, "Siames"); // Crear una instancia de la clase Gato
 miGato1.maullar(); // Llamar al método maullar
 miGato1.presentar(); // Llamar al método presentar
+console.log("");
 
 let miGato2 = new Gato("Mimi", 3, "Persa"); // Crear otra instancia de la clase Gato
 miGato2.maullar(); // Llamar al método maullar  
 miGato2.presentar(); // Llamar al método presentarse
+miGato2.setRaza("Bengalí"); // Cambiar la raza del gato utilizando el método setRaza
+console.log("");
+
+//Getter y Setter
+console.log(miGato1.getRaza()); // Llamar al método getRaza para obtener la raza del gato
+console.log(miGato2.getRaza()); // Llamar al método getRaza para obtener la raza del gato
+console.log("");
+
+miGato1.guardarEdad = 4; // Cambiar la edad del gato utilizando el setter
+console.log(miGato1.sacarEdad); // Llamar al método getRaza para obtener la raza del gato
