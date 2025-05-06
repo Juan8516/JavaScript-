e4a7e56adf077c1bcffd169a5f3343d285a55bfd
Juan8516 <juan.ind85@gmail.com>
2025-05-05 20:27:59 -0500

    Ejercicios 39 al 43
    - Preparacion de archivos

---
+++ 28.1_ejercicios-Bloque-5/script.js
@@ -0,0 +1,21 @@
+/*
+!!!Ejericicio de javaScript!!!
+
+Crea una aplicacion web en la que puedas añadir, mostrar y eliminar tus peliculas favoritas
+
+cada pelicula que guardes debe tener:
+
+-Titulo
+-fecha de guardado
+-puntuacion de popularidad (valor aleatorio entre 1 y 100)
+
+Las peliculas debe mantenerse guardadas aunque se recargue la pagina
+y se debe eliminar cualquiera de ellas de la lista de peliculas guardadas
+
+Tareas:
+-Ejercicio 39: Crear el formulario necesario para añadir una pelicula
+-Ejercicio 40: Funcion para guardar la pelicula con los datos necesarios
+-Ejercicio 41: Funcion para mostrar peliculas extraidos del localStorage
+-Ejercicio 42: Haz que las peliculas carguen automaticamente al abrir la web
+-Ejercicio 43: Funcion para eliminar peliculas.
+*/
