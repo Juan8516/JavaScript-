4a4ccd5aee0bf0422f738990c9033d6843f43750
Juan8516 <juan.ind85@gmail.com>
2025-05-06 20:01:00 -0500

    Fetch
    - Preparacion de archivos

---
+++ 30-fetch/script.js
@@ -0,0 +1,3 @@
+//Fetch: Es una API moderna de JavaScript para hacer peticiones HTTP (GET, POST, PUT, DELETE, etc.) y manejar 
+// respuestas de forma asíncrona. Devuelve una Promesa que resuelve en la respuesta del servidor.
+
