51f22bb29d1936e23ef5d0b9b1596916b170579f
Juan8516 <juan.ind85@gmail.com>
2025-05-07 20:50:33 -0500

    Fetch
    - Ajax
    - Sintaxis con promesas async y await

--- 30-fetch/script.js
+++ 30-fetch/script.js
@@ -1,13 +1,13 @@
 //Fetch: Es una API moderna de JavaScript para hacer peticiones HTTP (GET, POST, PUT, DELETE, etc.) y manejar 
 // respuestas de forma asíncrona. Devuelve una Promesa que resuelve en la respuesta del servidor.
 
-
+//Sacar datos de un backend (API) y mostrarlos en el frontend (HTML).
 fetch("https://reqres.in/api/users?page=2") // URL de la API
     .then(response => response.json()) // Convierte la respuesta a JSON
     .then(usuario => {
         
         console.log(usuario.data); // Muestra el objeto de usuario en la consola
-        mostrarUsuario(usuario.data[5]); // Llama a la función mostrarUsuario con el primer usuario
+        mostrarUsuario(usuario.data[4]); // Llama a la función mostrarUsuario con el primer usuario
 
     })
     .catch(error => {
@@ -22,3 +22,44 @@ function mostrarUsuario(user) {
         <img src="${user.avatar}" />
     `;
 }
+
+//Guardar datos en un backend (API) y mostrarlos en el frontend (HTML).
+const guardarDatos =  {
+
+    "name": "Juan Manuel Salazar",
+    "job": "Desarrollador web"
+};
+
+// fetch("https://reqres.in/api/users", {
+//     method: "POST", // Método de la petición
+//     headers: {
+//         "Content-Type": "application/json" // Tipo de contenido
+//     },
+//     body: JSON.stringify(guardarDatos) // Convierte el objeto a JSON
+
+// })
+// .then(response => response.json()) // Convierte la respuesta a JSON
+// .then(data => {
+//     console.log(data); // Muestra la respuesta en la consola
+// })
+// .catch(error => {
+//     console.error("Error:", error); // Manejo de errores
+// });
+
+//Sintaxis con promesas async/await:Es una forma moderna de trabajar con promesas en JavaScript, permitiendo escribir 
+// código asíncrono de manera más clara y secuencial (como si fuera síncrono)
+async function sacarUsuarios() {
+
+    try {
+        let response = await fetch("https://reqres.in/api/users?page=2"); // Espera la respuesta de la API
+        let data = await response.json(); // Convierte la respuesta a JSON
+
+        data.data.forEach(user => {
+            console.log(user); // Muestra cada usuario en la consola
+        });
+    }catch(error) {
+        console.error("El error atrapado es:", error); // Manejo de errores
+    }
+}
+
+sacarUsuarios(); // Llama a la función sacarUsuarios
